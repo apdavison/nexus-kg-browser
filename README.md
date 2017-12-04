d6a7bba18a790361be08afb4c95005c495fdafe3
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2017-12-04 23:15:42 +0100

    Added test scaffold

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Simple Nexus Knowledge Graph browser
 
 - also see https://github.com/BlueBrain/nexus-navigator-webapp
+
+
+To install dependencies:
+
+    npm install
+
+To run tests:
+
+    karma start
---
+++ app/services.spec.js
@@ -0,0 +1,47 @@
+/*
+
+
+*/
+
+
+describe('PathHandler service', function() {
+
+    var PathHandler;
+
+    beforeEach(angular.mock.module('nar'));
+
+    beforeEach(inject(function(_PathHandler_) {
+        PathHandler = _PathHandler_;
+    }));
+
+    it('should exist', function() {
+        expect(PathHandler).toBeDefined();
+    });
+
+    describe('.extract_path_from_uri()', function() {
+        it('should exist', function() {
+            expect(PathHandler.extract_path_from_uri).toBeDefined();
+        });
+        it('should parse out type and instance parts', function() {
+            expect(PathHandler.extract_path_from_uri('https://nexus.humanbrainproject.org/v0/data/bbp/experiment/patchedcellcollection/v0.1.0/3ce21161-7c08-44eb-b367-f01ed1e891be'))
+            .toEqual({
+                type: '/bbp/experiment/patchedcellcollection',
+                id: 'bbp/experiment/patchedcellcollection/v0.1.0/3ce21161-7c08-44eb-b367-f01ed1e891be'
+            });
+        });
+    });
+
+    describe('.extract_path_from_location()', function() {
+        it('should exist', function() {
+            expect(PathHandler.extract_path_from_location).toBeDefined();
+        });
+        it('should parse out type and set instance to null', function() {
+            expect(PathHandler.extract_path_from_location('/bbp/experiment/patchedcellcollection'))
+            .toEqual({
+                type: '/bbp/experiment/patchedcellcollection',
+                id: null
+            });
+        });
+    });
+
+});
